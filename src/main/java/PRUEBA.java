40a151fbed781e8f6ecdbac0499b6fa9e1016bc0
jvallejo18 <jvallejo18@jaumebalmes.net>
2019-10-09 15:23:59 +0200

    PRUEBA 2

--- src/main/java/PRUEBA.java
+++ src/main/java/PRUEBA.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import sun.jvmstat.perfdata.monitor.PerfStringVariableMonitor;
 
 public class PRUEBA {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
 }
